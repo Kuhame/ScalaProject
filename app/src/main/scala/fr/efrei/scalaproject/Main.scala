c2a7e9c1c84fff34d972eed9047bf9195271a92b
PHE <119488291+Pierre-He@users.noreply.github.com>
2024-07-04 17:32:35 +0200

    working form navigation (no logic yet)

--- app/src/main/scala/fr/efrei/scalaproject/Main.scala
+++ app/src/main/scala/fr/efrei/scalaproject/Main.scala
@@ -1,21 +1,71 @@
 
 import zio._
-
+import zio.Console._
 
 object Main extends ZIOAppDefault {
     def run =
-    //Console.printLine("Hello, World!")
-    /*  Console.printLine(
-      """This is line 1.
-        |This is line 2.
-        |This is line 3.""".stripMargin
-    ) */
-    for {
-        _ <- Console.printLine("What do you want to do ?")
-        _ <- Console.printLine("A. Create blank")
-        _ <- Console.printLine("B. Use existing graph")
-        firstChoice <- Console.readLine
-        _ <- Console.printLine(s"You have chosen $firstChoice!")
-    } yield()
+      for {
+          // Menu A-B
+          _ <- Console.printLine("What do you want to do ?")
+          _ <- Console.printLine("A. Create blank")
+          _ <- Console.printLine("B. Use existing graph")
+
+          firstChoice <- Console.readLine
+          _ <- handleFirstChoice(firstChoice)
+      } yield()
       
+    // Handler of Menu A-B
+    def handleFirstChoice(choice: String): UIO[Unit] = choice.trim.toUpperCase match {
+    case "A" => handleCreateBlankGraph
+    //case "B" => handleUseExistingGraph
+    case _   => Console.printLine(s"Invalid choice: $choice").orDie
+    }
+
+
+    // A. Create a blank Graph 
+    def handleCreateBlankGraph: UIO[Unit] = 
+    for {
+        _ <- Console.printLine(
+        """A-1: What to do?
+          |1. Get all Vertices
+          |2. Get all Edges
+          |3. Get neighbors of a Vertex
+          |4. Add Edge
+          |5. Remove Edge
+          |6. Save as Graph (DOT + JSON)
+        """.stripMargin
+      ).orDie
+      subChoice <- Console.readLine.orDie
+      _ <- blankMenu(subChoice)
+    } yield()
+
+    // Handler of Menu A
+    def blankMenu(choice: String): UIO[Unit] = choice.trim match {
+      case "1" => getAllVertices *> handleCreateBlankGraph
+      case "2" => getAllEdges *> handleCreateBlankGraph
+      case "3" => getNeighborsOfVertex *> handleCreateBlankGraph
+      case "4" => addEdge *> handleCreateBlankGraph
+      case "5" => removeEdge  *> handleCreateBlankGraph
+      case "6" => saveGraph 
+      case _   => Console.printLine(s"Invalid choice: $choice").orDie *> handleCreateBlankGraph
+    }
+
+    // Handler of Menu A-1 : What to Do ?
+    def getAllVertices: UIO[Unit] =
+      Console.printLine("Fetching all vertices...").orDie
+
+    def getAllEdges: UIO[Unit] =
+      Console.printLine("Fetching all edges...").orDie
+
+    def getNeighborsOfVertex: UIO[Unit] =
+      Console.printLine("Fetching neighbors of a vertex...").orDie
+
+    def addEdge: UIO[Unit] =
+      Console.printLine("Adding an edge...").orDie
+
+    def removeEdge: UIO[Unit] =
+      Console.printLine("Removing an edge...").orDie
+
+    def saveGraph: UIO[Unit] =
+      Console.printLine("Saving the graph as DOT and JSON...").orDie
 }
